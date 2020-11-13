706d413a68793d81021ef33f871c576abf61edd4
akbarlisamir <samir90319@gmail.com>
2020-11-13 04:31:41 +0400

    Topic Tag Entities changes

--- src/main/java/hu/elte/softech/entity/Topic.java
+++ src/main/java/hu/elte/softech/entity/Topic.java
@@ -8,11 +8,15 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity
@@ -33,5 +37,11 @@ public class Topic {
     
     @OneToMany(mappedBy="topic")
     private List<Entry> entries;
+    
+    @ManyToMany
+    @JoinTable(name = "TopicTag", 
+    		  joinColumns = @JoinColumn(name = "tag_id"), 
+    		  inverseJoinColumns = @JoinColumn(name = "topic_id"))
+    private Set<Tag> tags;
     
 }
